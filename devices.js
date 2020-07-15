39450975e4f1860678dd28125dc0e4d9b58de614
Sergey Koptyakov <52034595+koptserg@users.noreply.github.com>
2020-07-16 01:53:07 +0500

    Create devices.js

---
+++ devices.js
@@ -0,0 +1,10 @@
+    {
+        zigbeeModel: ['Real_1'],
+        model: 'Real_1',
+        vendor: 'Real',
+        description: '',
+        supports: 'on/off',
+        fromZigbee: [fz.on_off, fz.generic_battery_voltage, fz.generic_battery],
+        fromZigbee: [fz.on_off, fz.battery, fz.temperature],
+        toZigbee: [tz.on_off],
+    },
