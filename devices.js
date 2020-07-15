49f48aae322ddf1dc23eca45ddecd4b90e9b587b
Sergey Koptyakov <52034595+koptserg@users.noreply.github.com>
2020-07-16 01:53:43 +0500

    Update devices.js

--- devices.js
+++ devices.js
@@ -4,7 +4,6 @@
         vendor: 'Real',
         description: '',
         supports: 'on/off',
-        fromZigbee: [fz.on_off, fz.generic_battery_voltage, fz.generic_battery],
         fromZigbee: [fz.on_off, fz.battery, fz.temperature],
         toZigbee: [tz.on_off],
     },
